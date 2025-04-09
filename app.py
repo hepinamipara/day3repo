6cbec6c6540ba644916a9bba359e94f0c5945c2d
root <root@ip-172-31-86-233.ec2.internal>
2025-04-09 10:12:05 +0000

    the chane in key name linuxkey by linuxdeveloper

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ client = boto3.client('ec2')
 
 response = client.run_instances(
     ImageId='ami-071226ecf16aa7d96',
-    KeyName='demokey',
+    KeyName='linuxkey',
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro'
