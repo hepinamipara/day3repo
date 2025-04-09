777e76353eae6764a589f24671138b9943e0dbce
hepinamipara <hepinamipara7600@gmail.com>
2025-04-09 15:33:04 +0530

    first time upload a file

---
+++ app.py
@@ -0,0 +1,11 @@
+import boto3
+
+client = boto3.client('ec2')
+
+response = client.run_instances(
+    ImageId='ami-071226ecf16aa7d96',
+    KeyName='demokey',
+    MinCount=1,
+    MaxCount=1,
+    InstanceType='t2.micro'
+)
